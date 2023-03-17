798093e92c1bd16e74f1652d03796418dfd5b186
理竣 莊 <andy28572463@gmail.com>
2023-03-17 10:54:20 +0800

    init: .net core mvc

---
+++ src/donet-mvc-vue-spa-tempalte/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using donet_mvc_vue_spa_tempalte.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace donet_mvc_vue_spa_tempalte.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/donet-mvc-vue-spa-tempalte/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace donet_mvc_vue_spa_tempalte.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
